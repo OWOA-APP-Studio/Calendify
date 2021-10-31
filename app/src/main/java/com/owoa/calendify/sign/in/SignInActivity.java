4e3a832a76988eff4f43b0ebf14059f2c77904fa
Haebun <mediafactory1279@gmail.com>
2021-10-31 20:23:51 +0900

    Sign In Activity 골자 작업

---
+++ app/src/main/java/com/owoa/calendify/sign/in/SignInActivity.java
@@ -0,0 +1,17 @@
+package com.owoa.calendify.sign.in;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.owoa.calendify.R;
+
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
