38f1149b5a5a27d1f62667313c20826634e0d2f4
이규현 <igh2932@gmail.com>
2021-11-04 14:24:50 +0900

    Revert "Revert "2차 수정""
    This reverts commit 4e7d9c459b533913bff812701216113fca150b0d.

--- app/src/main/java/com/owoa/calendify/schedule/ScheduleCreateActivity.java
+++ app/src/main/java/com/owoa/calendify/schedule/ScheduleCreateActivity.java
@@ -14,20 +14,26 @@ import android.widget.Toast;
 
 import com.owoa.calendify.R;
 
-public class ScheduleCreateActivity extends AppCompatActivity {
+public class ScheduleCreateActivity extends AppCompatActivity implements Contract.View {
 
     private Spinner spinner;
     private TextView tv_result;
     private Button button;
     private EditText sample_Name , sample_detail;
+    private Contract.Presenter ScheduleCreatePresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule_create);
+        ScheduleCreatePresenter = new ScheduleCreatePresenter((Contract.View) this);
+        init();
+    }
 
-        spinner = (Spinner)findViewById(R.id.spinner);
-        tv_result = (TextView)findViewById(R.id.tv_result);
+
+    protected void init() {
+        spinner = (Spinner) findViewById(R.id.spinner);
+        tv_result = (TextView) findViewById(R.id.tv_result);
         button = (Button) findViewById(R.id.add_button);
         sample_Name = (EditText) findViewById(R.id.sample_Name);
         sample_detail = (EditText) findViewById(R.id.sample_Detail);
@@ -44,7 +50,6 @@ public class ScheduleCreateActivity extends AppCompatActivity {
             }
         });
 
-        Intent i = new Intent(ScheduleCreateActivity.this,ScheduleCreateModel.class);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,16 +57,16 @@ public class ScheduleCreateActivity extends AppCompatActivity {
                 String name = sample_Name.getText().toString();
                 String detail = sample_detail.getText().toString();
                 String category = tv_result.getText().toString();
-
-                Bundle bundle = new Bundle();
-                bundle.putString("name",name);
-                bundle.putString("detail",detail);
-                bundle.putString("category",category);
-                i.putExtras(bundle);
-                startActivity(i);
-
+                ScheduleCreatePresenter.get(name,detail,category);
+                ScheduleCreateModel.saveData(tv_result);
             }
         });
-
     }
+
+    @Override
+    public void showResult(String name, String detail, String category) {
+        Toast.makeText(getApplicationContext(),(name + "\n" + detail + "\n" + category),Toast.LENGTH_LONG).show();
+//        ((TextView)findViewById(R.id.title)).setText(answer + num2 + num3);
+    }
+
 }
---
+++ app/src/main/java/com/owoa/calendify/schedule/ScheduleCreatePresenter.java
@@ -0,0 +1,16 @@
+package com.owoa.calendify.schedule;
+
+public class ScheduleCreatePresenter implements Contract.Presenter {
+    Contract.View view;
+    ScheduleCreateModel scheduleCreateModel;
+    public ScheduleCreatePresenter(Contract.View view){
+        this.view = view;                   //Activty View정보 가져와 통신
+        scheduleCreateModel = new ScheduleCreateModel(this);    //Model 객체 생성
+    }
+
+    //Presenter를 상속하고 addNum 구현
+    @Override
+    public void get(String name, String detail, String category) {
+        view.showResult(name,detail,category);
+    }
+}
