4e7d9c459b533913bff812701216113fca150b0d
이규현 <igh2932@gmail.com>
2021-11-04 14:24:09 +0900

    Revert "2차 수정"
    This reverts commit fd0d7c2e514c1826623bdaee247892b1085a1fed.

--- app/src/main/java/com/owoa/calendify/schedule/ScheduleCreateActivity.java
+++ app/src/main/java/com/owoa/calendify/schedule/ScheduleCreateActivity.java
@@ -14,26 +14,20 @@ import android.widget.Toast;
 
 import com.owoa.calendify.R;
 
-public class ScheduleCreateActivity extends AppCompatActivity implements Contract.View {
+public class ScheduleCreateActivity extends AppCompatActivity {
 
     private Spinner spinner;
     private TextView tv_result;
     private Button button;
     private EditText sample_Name , sample_detail;
-    private Contract.Presenter ScheduleCreatePresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule_create);
-        ScheduleCreatePresenter = new ScheduleCreatePresenter((Contract.View) this);
-        init();
-    }
 
-
-    protected void init() {
-        spinner = (Spinner) findViewById(R.id.spinner);
-        tv_result = (TextView) findViewById(R.id.tv_result);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        tv_result = (TextView)findViewById(R.id.tv_result);
         button = (Button) findViewById(R.id.add_button);
         sample_Name = (EditText) findViewById(R.id.sample_Name);
         sample_detail = (EditText) findViewById(R.id.sample_Detail);
@@ -50,6 +44,7 @@ public class ScheduleCreateActivity extends AppCompatActivity implements Contrac
             }
         });
 
+        Intent i = new Intent(ScheduleCreateActivity.this,ScheduleCreateModel.class);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,16 +52,16 @@ public class ScheduleCreateActivity extends AppCompatActivity implements Contrac
                 String name = sample_Name.getText().toString();
                 String detail = sample_detail.getText().toString();
                 String category = tv_result.getText().toString();
-                ScheduleCreatePresenter.get(name,detail,category);
-                ScheduleCreateModel.saveData(tv_result);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("name",name);
+                bundle.putString("detail",detail);
+                bundle.putString("category",category);
+                i.putExtras(bundle);
+                startActivity(i);
+
             }
         });
-    }
 
-    @Override
-    public void showResult(String name, String detail, String category) {
-        Toast.makeText(getApplicationContext(),(name + "\n" + detail + "\n" + category),Toast.LENGTH_LONG).show();
-//        ((TextView)findViewById(R.id.title)).setText(answer + num2 + num3);
     }
-
 }
--- app/src/main/java/com/owoa/calendify/schedule/ScheduleCreateModel.java
+++ app/src/main/java/com/owoa/calendify/schedule/ScheduleCreateModel.java
@@ -1,18 +1,28 @@
 package com.owoa.calendify.schedule;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ScheduleCreateModel {
-    Contract.Presenter presenter;
-    public ScheduleCreateModel(Contract.Presenter presenter){
-        this.presenter = presenter;
-    }
+import androidx.appcompat.app.AppCompatActivity;
 
-    public static void saveData(TextView tv_result) {
-    }
+import com.owoa.calendify.R;
 
-    public void saveData(String data){
-        //처리 로직
-        String name = data;
+public class ScheduleCreateModel extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_create);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        String name = bundle.getString("name");
+        String detail = bundle.getString("detail");
+        String category = bundle.getString("category");
+
+        Toast.makeText(getApplicationContext(),(name + "\n" + detail + "\n" + category),Toast.LENGTH_LONG).show();
+
     }
 }
