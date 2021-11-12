73cfbb89f38b8a2a7a19f4ad85b8e14e110f2658
이규현 <igh2932@gmail.com>
2021-11-12 11:12:06 +0900

    일정 작성 메인 연결

--- app/src/main/java/com/owoa/calendify/schedule/read/ScheduleReadActivity.java
+++ app/src/main/java/com/owoa/calendify/schedule/read/ScheduleReadActivity.java
@@ -2,9 +2,14 @@ package com.owoa.calendify.schedule.read;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.owoa.calendify.R;
+import com.owoa.calendify.schedule.ScheduleCreateActivity;
 
 public class ScheduleReadActivity extends AppCompatActivity {
 
@@ -12,5 +17,13 @@ public class ScheduleReadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_read);
+        ImageView  scheduleCreateButton = (ImageView) findViewById(R.id.create_schedule_button);
+        scheduleCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ScheduleReadActivity.this, ScheduleCreateActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
