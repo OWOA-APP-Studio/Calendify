8ae78c04339e1e38ffc369f440cfb0bddc84b225
이규현 <igh2932@gmail.com>
2021-11-14 13:28:56 +0900

    일정 수정 레이아웃 추가

--- app/src/main/java/com/owoa/calendify/schedule/Update/ScheduleUpdateActivity.java
+++ app/src/main/java/com/owoa/calendify/schedule/Update/ScheduleUpdateActivity.java
@@ -1,4 +1,29 @@
 package com.owoa.calendify.schedule.Update;
 
-public class ScheduleUpdateActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.owoa.calendify.R;
+import com.owoa.calendify.schedule.Contract;
+import com.owoa.calendify.schedule.ScheduleCreatePresenter;
+
+public class ScheduleUpdateActivity extends AppCompatActivity {
+
+    private EditText sample_Name , sample_detail, sample_location;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_update);
+
+        sample_Name = (EditText) findViewById(R.id.sample_Name);
+
+    }
 }
+
