f08b2c683fb941d9b22fdae8e015aa6f3b4066b8
이규현 <igh2932@gmail.com>
2021-11-19 09:35:10 +0900

    read 일정 추가 버튼 이미지 추가

--- app/src/main/java/com/owoa/calendify/schedule/read/ScheduleReadActivity.java
+++ app/src/main/java/com/owoa/calendify/schedule/read/ScheduleReadActivity.java
@@ -40,5 +40,7 @@ public class ScheduleReadActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
 }
--- app/src/main/java/com/owoa/calendify/server/ServerData.java
+++ app/src/main/java/com/owoa/calendify/server/ServerData.java
@@ -4,5 +4,5 @@ public class ServerData {
     public static String IP = "http://182.228.83.7";
     public static String LOCALHOST = "http://192.168.219.110";
 
-    public static String URL = LOCALHOST + "/Calendify/";
+    public static String URL = IP + "/Calendify/";
 }
