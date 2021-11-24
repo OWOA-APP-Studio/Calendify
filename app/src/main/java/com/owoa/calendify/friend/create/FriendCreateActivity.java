34649c32845224cf0a54817e3bd7277a8f9d2d95
이규현 <igh2932@gmail.com>
2021-11-24 23:03:16 +0900

    친구추가 팝업 창에서 친구 목록으로 데이터 전달 완료

--- app/src/main/java/com/owoa/calendify/friend/create/FriendCreateActivity.java
+++ app/src/main/java/com/owoa/calendify/friend/create/FriendCreateActivity.java
@@ -1,30 +1,66 @@
 package com.owoa.calendify.friend.create;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.owoa.calendify.R;
 
-public class FriendCreateActivity extends AppCompatActivity {
+import com.owoa.calendify.friend.read.FriendReadActivity;
+
+//class Code {
+//    public static int requestCode = 100;
+//    public static int resultCode = 1;
+//}
+public class FriendCreateActivity extends Activity {
+
+    EditText txtText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        //타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_friend_add);
+
+        //UI 객체생성
+        txtText = (EditText)findViewById(R.id.txtText);
+
+    }
+
+    //확인 버튼 클릭
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("name", txtText.getText().toString());
+        setResult(RESULT_OK, resultIntent);
+        //액티비티(팝업) 닫기
+        finish();
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if( event.getAction() == MotionEvent.ACTION_OUTSIDE ) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
             return false;
         }
         return true;
     }
 
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
 }
--- app/src/main/java/com/owoa/calendify/friend/read/FriendReadActivity.java
+++ app/src/main/java/com/owoa/calendify/friend/read/FriendReadActivity.java
@@ -2,8 +2,10 @@ package com.owoa.calendify.friend.read;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,16 +13,36 @@ import com.owoa.calendify.R;
 import com.owoa.calendify.friend.create.FriendCreateActivity;
 import com.owoa.calendify.schedule.read.ScheduleReadActivity;
 
+//class Code {
+//    public static int requestCode = 100;
+//    public static int resultCode = 1;
+//}
 public class FriendReadActivity extends AppCompatActivity {
+    TextView txtResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+        txtResult = (TextView)findViewById(R.id.txtResult);
 
         ImageView friendListButton = findViewById(R.id.friend_add_button);
         friendListButton.setOnClickListener( view -> {
-            Intent intent1 = new Intent(FriendReadActivity.this, FriendCreateActivity.class);
-            startActivity(intent1);
+            Intent intent = new Intent(FriendReadActivity.this, FriendCreateActivity.class);
+            startActivityForResult(intent,1);
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //데이터 받기
+                String result = data.getStringExtra("name");
+                txtResult.setText(result);
+            }
+        }
+    }
+
 }
