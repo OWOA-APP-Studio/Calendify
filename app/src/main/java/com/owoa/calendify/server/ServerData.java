7d6ed45705e39832630d538d71c3e6182c21026d
Haebun <mediafactory1279@gmail.com>
2021-12-05 15:31:09 +0900

    ServerData에 LocalHost 비활성화

--- app/src/main/java/com/owoa/calendify/server/ServerData.java
+++ app/src/main/java/com/owoa/calendify/server/ServerData.java
@@ -2,7 +2,7 @@ package com.owoa.calendify.server;
 
 public class ServerData {
     public static String IP = "http://182.228.83.7";
-    public static String LOCALHOST = "http://192.168.219.110";
+//    public static String LOCALHOST = "http://192.168.219.110";
 
-    public static String URL = LOCALHOST + "/Calendify/";
+    public static String URL = IP + "/Calendify/";
 }
